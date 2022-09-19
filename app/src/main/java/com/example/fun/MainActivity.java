6d6972b14bd3dd49a12db6e8fb7bcf703c6de89d
Anton Divavin <ADIVAVIN@MAIL.RU>
2022-09-19 22:38:07 +0300

    test

--- app/src/main/java/com/example/fun/MainActivity.java
+++ app/src/main/java/com/example/fun/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.fun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -12,12 +15,9 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     private ImageButton buttonStart;
     private TextView showTextView;
-
-
-
-    Random random = new Random();
-    int amountCup = random.nextInt(11);
-
+    public ImageButton buttonStartGame;
+    private TextView textCup;
+    int test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +25,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         buttonStart = findViewById(R.id.startButton);
         showTextView = findViewById(R.id.showText);
+        textCup = findViewById(R.id.textcup);
+        buttonStartGame = findViewById(R.id.startgamebutton);
+        MediaPlayer speakMalo = MediaPlayer.create(this, R.raw.malo1);
+        MediaPlayer speakKakEubu = MediaPlayer.create(this, R.raw.kakeubu);
+        MediaPlayer primatWin = MediaPlayer.create(this, R.raw.primatwin);
+        Random random = new Random();
+        String[] arraysQuote = getResources().getStringArray(R.array.qoute);
+        int sizeArrayQoute = arraysQuote.length;
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int amountCup = random.nextInt(11);
+                int randomResFromArrays = random.nextInt(sizeArrayQoute);
+                if (amountCup == 0) { textCup.setText(String.valueOf(amountCup) + "" + "! хуй тебе");}
+                if (amountCup == 1) { textCup.setText(String.valueOf(amountCup) + " " + "стакан!") ;}
+                if ((amountCup > 2) && (amountCup <= 4) ) { textCup.setText(String.valueOf(amountCup) + " " + "стаканa!") ;}
+                if (amountCup > 5)  { textCup.setText(String.valueOf(amountCup) + " " + "стаканов!") ;}
+//test
+                if (amountCup <= 5){ speakMalo.start();}
+                if (amountCup == 10) { primatWin.start();}
+                else  if ((amountCup >= 6) && (amountCup < 10) ) {speakKakEubu.start();}
+                showTextView.setText(arraysQuote[randomResFromArrays]);
+            }
+        });
+
+            }
+
+    public void startGame(View view) {
+            switch (view.getId()){
+                case R.id.startgamebutton:
+                    Intent startGameIntent = new Intent(this, GameActivity.class);
+                    startActivity(startGameIntent);
+                    break;
+                default:
+                    break;
+            }
+
+
     }
-
-
-
-
 }
+
+
+
+
